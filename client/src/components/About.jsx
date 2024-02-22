38088a61673f7b0f2a7a73a8208e730be087c879
agusvlopez <agusvlopez@gmail.com>
2024-02-22 16:39:33 -0300

    add: about section

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,9 +1,34 @@
+import aboutMe1 from '../covers/me--about-1.jpg';
+import aboutMe2 from '../covers/me--about-2.jpg';
+import aboutMe3 from '../covers/me--about-3.jpg';
 
 function About() {
 
     return (
         <>
-            <h2 className='home--subtitle'>Sobre Hera</h2>
+            <section className="container max-w-5xl mx-auto mt-6 mb-6 p-4 pt-6 gap-8 h-full md:h-screen">
+                <div id="about" className="p-2 section-about md:flex md:gap-6">
+                    <div className="section-about--text">
+                        <h2 className='home--subtitle mb-4'>Sobre Hera</h2>
+                        <p>¡Hola, <span className='font-bold'>soy Patri!</span></p>
+
+                        <p>¡Bienvenidos a <strong>Hera</strong>! Un espacio en donde la confidencialidad es absoluta. Hago <strong>Lecturas de Tarot</strong>, <strong>Ayuda espiritual con velas</strong> y <strong>Reiki</strong>.</p>
+
+                        <p>Mi lema es que siempre se trasmita <span className='font-bold'>mucha luz</span>, <span className='font-bold'>buena energía</span>, <span className='font-bold'>sinceridad</span> y <span className='font-bold'>transparencia</span>. Lo cual aplico en cada <strong>Lectura de Tarot</strong>, <strong>Ayuda espiritual</strong> y <strong>Reiki</strong>.</p>
+                    </div>
+                    <div class="section-about--images">
+                        <div class="images-container">
+                            <img src={aboutMe1} alt="Patri con las cartas del Tarot." class="image-3" />
+                        </div>
+                        <div class="images-container">
+                            <img src={aboutMe2} alt="Patri con las cartas del Tarot." class="image-2" />
+                        </div>
+                        <div class="images-container">
+                            <img src={aboutMe3} alt="Patri con las cartas del Tarot." class="image-1" />
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,13 +3,15 @@ import About from "../components/About";
 export default function HomePage() {
     return (
         <>
-            <div className='max-w-screen md:flex md:justify-between'>
-                <span className='sunLeft--image'>
-                    <div className='p-4 pt-0 text-center h-full w-screen flex flex-col justify-center md:mt-[-5%]'>
-                        <h1 className='home--title md:self-center'>Hera</h1>
-                        <span className='md:self-center home--description'>Tarot, Ayuda espiritual con velas y Reiki</span>
-                    </div></span>
-                <span className='sunRight--image'></span>
+            <div className='max-w-screen relative'>
+                <span className='sunLeft--image absolute top-0 left-0'></span>
+                <div className='flex justify-center items-center h-screen'>
+                    <div className='text-center mt-[-10%]'>
+                        <h1 className='home--title'>Hera</h1>
+                        <span className='home--description'>Tarot, Ayuda espiritual con velas y Reiki</span>
+                    </div>
+                </div>
+                <span className='sunRight--image absolute top-0 right-0'></span>
             </div>
             <div className='mt-8 h-screen'>
                 <About />
